6b5a64734aca3e044b430a5f2ad5a45c7f95d5e9
NaeemSlayer <muhammedalam150@gmail.com>
2025-08-30 11:14:42 +0500

    feat: logout function

--- src/other/Header.jsx
+++ src/other/Header.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { setLocalStorage } from "../utils/localStorage";
 
 const Header = () => {
+  const logOutUser = () => {
+    localStorage.setItem("loggedInUser", "");
+    window.location.reload();
+  };
   return (
     <div className="flex items-end justify-between">
       <h1 className="text-2xl font-semibold">
-        Hello <br />{" "}
-        <span className="text-3xl font-semibold">Admin 👋🏻</span>
+        Hello <br /> <span className="text-3xl font-semibold">Admin 👋🏻</span>
       </h1>
-      <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+      <button
+        onClick={logOutUser}
+        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+      >
         Log Out
       </button>
     </div>
